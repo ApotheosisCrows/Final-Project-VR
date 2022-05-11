dbddc25b8f8a4998786b9f49cf2f82d28fe21f94
unitednations1235 <24pattn@live.elida.oh.us>
2022-05-11 09:59:48 -0400

    5/11
    yes

--- Assets/_Course Library/detect_collisions.cs
+++ Assets/_Course Library/detect_collisions.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class detect_collisions : MonoBehaviour
 {
+    private Vector3 newSpawnLocation;
+    public GameObject spawn1;
+    public GameObject spawn2;
+    public GameObject spawn3;
+    public GameObject mob1;
+    //public GameObject mob2;
+    //public GameObject mob3;
+
+    Vector3[] spawnLocations = new Vector3[3];
     // Start is called before the first frame update
     void Start()
     {
-
+        mob1 = this.gameObject;
+        spawnLocations[0] = spawn1.transform.position;
+        spawnLocations[1] = spawn2.transform.position;
+        spawnLocations[2] = spawn3.transform.position;
+        Debug.Log("Spawnloc0: " + spawnLocations[0]);
+        
     }
 
     // Update is called once per frame
@@ -18,7 +32,14 @@ public class detect_collisions : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Destroy(gameObject);
+        if(other.gameObject.CompareTag("Projectile"))
+        
+           
         Destroy(other.gameObject);
+        int picked = Random.Range(0, spawnLocations.Length);
+        newSpawnLocation = spawnLocations[picked];
+        Debug.Log("NewSpawnLocation: " + newSpawnLocation);
+        Instantiate(mob1, newSpawnLocation, Quaternion.identity);
+        Destroy(this.gameObject);
     }
 }
